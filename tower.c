274fc919c2c7a71b128d5db17b0076eb67070e1d
11249a218-arch <11249a218@kanchiuniv.ac.in>
2026-02-12 12:02:24 +0530

    tower.c

---
+++ tower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void towers(int num, char source, char dest, char spare);
+int main() {
+    int num;
+    printf("Enter the number of disks: ");
+    scanf("%d", &num);
+    printf("\nThe sequence of moves involved in the Tower of Hanoi:\n");
+    towers(num, 'A', 'C', 'B');
+    return 0;
+}
+void towers(int num, char source, char dest, char spare) {
+    if (num == 1) {
+        printf("Move disk 1 from %c to %c\n", source, dest);
+        return;
+    }
+    towers(num - 1, source, spare, dest);
+    printf("Move disk %d from %c to %c\n", num, source, dest);
+    towers(num - 1, spare, dest, source);
+}
